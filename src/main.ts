4c8a9d2f6b48a966647d0de78eb5ff9b124c24c4
Yono <yono@ideku.id>
2025-01-29 02:22:44 +0700

    use nest 11

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config'; // Import ConfigService
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Get the port from the environment variables
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3000; // Default to 3000 if not set in .env
-
-  // Start the server
-  await app.listen(port);
-  Logger.log(`User Service is running on: http://localhost:${port}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
